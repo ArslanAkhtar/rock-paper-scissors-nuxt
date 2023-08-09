485625f89e21316ba751330b59adf3782f4f8306
Muhammad Arslan <muhammad.arslan@iodigital.com>
2023-08-09 16:01:49 +0200

    Changes push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['vuetify/styles/main.sass', '@mdi/font/css/materialdesignicons.css'],
-  components: ['~/components'],
   build: {
     transpile: ['vuetify', 'gsap']
   },
   devtools: { enabled: true },
   modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss'],
 
-  imports: {
-    dirs: [
-      // Scan top-level modules
-      'composables',
-      // ... or scan modules nested one level deep with a specific name and file extension
-      'composables/*/index.{ts,js,mjs,mts}',
-      // ... or scan all modules within given directory
-      'composables/**'
-    ]
-  }
+
 })
