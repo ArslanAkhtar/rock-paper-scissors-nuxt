43f3bf105355ed649132febc219f9a0b185b2c95
Soheil Movahedan <soheil.movahhedan@gmail.com>
2023-08-15 15:00:13 +0200

    Try to get room data in play page (WIP)

--- pages/play.vue
+++ pages/play.vue
@@ -1,6 +1,34 @@
 <script lang="ts" setup>
+const route = useRoute()
+const isSinglePlayer = computed(() => !readFirstQ(route, 'id'))
+
 const { chooseOption, playAgain, playerScore, computerOption, result } =
   gameLogic()
+const { makeChoice, resetGame } = multiplayerLogic()
+
+const { load, result: statusResult } = useLazyQuery(
+  gql`
+    query CurrentStatus($roomId: ID!) {
+      currentStatus(roomId: $roomId) {
+        playerChoices {
+          PlayerId
+        }
+      }
+    }
+  `
+)
+
+onMounted(() => {
+  const id = readFirstQ(route, 'id')
+  if (id) {
+    load(undefined, { roomId: id })
+  }
+})
+
+console.log({ statusResult })
+
+const choose = computed(() => (isSinglePlayer ? chooseOption : makeChoice))
+const reset = computed(() => (isSinglePlayer ? playAgain : resetGame))
 </script>
 
 <template>
@@ -9,8 +37,8 @@ const { chooseOption, playAgain, playerScore, computerOption, result } =
     <GameBoard
       :houseSelection="(computerOption as string)"
       :result="(result as string)"
-      @chooseOption="chooseOption"
-      @playerScore="playAgain"
+      @chooseOption="choose"
+      @playAgain="reset"
     />
   </div>
 </template>
