cf2770566ceeb330afe526a1d2522b3a1b8b9ae9
Soheil Movahedan <soheil.movahhedan@gmail.com>
2023-08-15 15:00:55 +0200

    Rewrite waiting-room with pinia store amd fix its logic

--- pages/waiting-room.vue
+++ pages/waiting-room.vue
@@ -1,112 +1,128 @@
 <script lang="ts" setup>
+import { uuid } from 'vue-uuid'
 import { useClipboard, usePermission } from '@vueuse/core'
 
 const router = useRouter()
 const route = useRoute()
 const config = useRuntimeConfig()
+const state = usePlayersStore()
 
-const { selfPlayer, otherPlayer, gameMode, meetingCode } =
-  injectPlayersContext()
-
-const iamTheOtherPlayer = computed(() => {
-  if (route.query.id && route.query.host) {
-    return true
-  }
-
-  return false
-})
-const selfPlayerTextInput = ref('')
-const setSelfPlayerName = () => {
-  selfPlayer.value = selfPlayerTextInput.value
-}
-
-const meetingLink = computed(
-  () =>
-    `${config.public.baseUrl}/waiting-room?id=${meetingCode.value}&host=${selfPlayer.value}`
+const isRoomCreated = ref(false)
+const isReadyToBeRedirected = ref(false)
+const isUserGuest = computed(
+  () => !!route.query.id && !!route.query.hostId
+)
+const waitingRoomLink = computed(
+  () => `${config.public.baseUrl}${state.waitingRoomLink}`
 )
 
 // Save the host name as other player when I'm joining his match
 watchEffect(() => {
-  if (iamTheOtherPlayer.value) {
-    selfPlayer.value = ''
-    otherPlayer.value = Array.isArray(route.query.host)
-      ? route.query.host[0] || ''
-      : route.query.host || ''
+  if (isUserGuest.value) {
+    state.setMatchCode(readFirstQ(route, 'id'))
+    state.setPlayer1('', '')
+    state.setPlayer2(readFirstQ(route, 'hostId'), readFirstQ(route, 'hostName'))
   }
 })
 
-// In case of user entered URL directly, put him in correct flow
+const { mutate: registerUser, onDone: onUserRegistered } = useMutation(gql`
+  mutation RegisterPlayer($playerId: ID!, $playerName: String!) {
+    registerPlayer(playerId: $playerId, playerName: $playerName)
+  }
+`)
+onUserRegistered(() => {
+  if (isUserGuest.value) {
+    isReadyToBeRedirected.value = true
+  } else {
+    isRoomCreated.value = true
+  }
+})
+
+const { mutate: createRoom, onDone: onRoomCreated } = useMutation(gql`
+  mutation CreateRoom($roomId: ID!) {
+    createRoom(roomId: $roomId)
+  }
+`)
+onRoomCreated(() => {
+  const selfId = uuid.v4()
+  state.setPlayer1(selfId, state.player1Name)
+  registerUser({ playerId: selfId, playerName: state.player1Name })
+})
+
+// Create room when user enters the page
 onMounted(() => {
-  if (!iamTheOtherPlayer.value && gameMode.value === 'single') {
-    router.push('/')
+  if (!isUserGuest.value) {
+    const newRoomId = uuid.v4()
+    state.setMatchCode(newRoomId)
+    createRoom({ roomId: newRoomId })
   }
 })
 
-// Copy the meeting link
+// Copy the room link
 const { isSupported, copy } = useClipboard()
 const permissionWrite = usePermission('clipboard-write')
-const copyMeetingCode = () => {
+const copyRoomLink = () => {
   if (permissionWrite.value === 'prompt') {
-    copy(meetingLink.value)
+    copy(waitingRoomLink.value)
   }
+}
+
+// Get guest name
+const guestPlayerTextInput = ref('')
+const setGuestReady = () => {
+  const selfId = uuid.v4()
+  state.setPlayer1(selfId, guestPlayerTextInput.value)
+  registerUser({ playerId: selfId, playerName: guestPlayerTextInput.value })
 }
 
 // Auto start section
 const secondsToStart = ref(3)
 const countDownToStartInterval = ref<NodeJS.Timer | null>(null)
 watchEffect(() => {
-  if (otherPlayer.value && selfPlayer.value) {
+  if (isReadyToBeRedirected.value) {
     countDownToStartInterval.value = setInterval(() => {
       if (secondsToStart.value > -1) {
         secondsToStart.value--
+      } else {
+        router.push(state.matchLink)
       }
     }, 1000)
   }
 })
 onBeforeUnmount(() => {
   if (countDownToStartInterval.value) {
+    isReadyToBeRedirected.value = false
     clearInterval(countDownToStartInterval.value)
   }
 })
-watchEffect(() => {
-  if (secondsToStart.value === -1) {
-    router.push('/play')
-  }
-})
-onBeforeUnmount(() => {
-  if (countDownToStartInterval.value) {
-    clearInterval(countDownToStartInterval.value)
-  }
-})
-
 </script>
 
 <template>
   <div
-    v-if="!iamTheOtherPlayer"
+    v-if="!isUserGuest"
     class="flex flex-col justify-center align-center max-w-xl w-full p-8"
   >
     <h1 class="text-2xl text-white">
-      Invite your friend {{ selfPlayer }}!
+      Invite your friend {{ state.player1Name }}!
     </h1>
 
     <div class="flex flex-col my-8 gap-4 p-4 bg-slate-600 rounded-md">
-      <p>{{ meetingLink }}</p>
-      <v-btn v-if="isSupported" @click="copyMeetingCode()">
+      <p>{{ state.waitingRoomLink }}</p>
+      <v-btn v-if="isSupported" @click="copyRoomLink()">
         Copy
       </v-btn>
       <span v-else>Copy the meeting code and give it to your friend</span>
     </div>
 
-    <h3 v-if="!otherPlayer">
+    <h3 v-if="!state.player2Name">
       As soon as your friend join the room, the match will be started
     </h3>
     <h3 v-else>
-      {{ otherPlayer }} joined! The match will be started in
+      {{ state.player2Name }} joined! The match will be started in
       {{ Math.max(secondsToStart, 0) }}...
     </h3>
 
-    <v-btn v-if="otherPlayer" to="/play">
+    <v-btn v-if="state.player2Name" to="/play">
       Start Now!
     </v-btn>
     <v-btn to="/">
@@ -114,22 +130,25 @@ onBeforeUnmount(() => {
     </v-btn>
   </div>
 
-  <div v-else class="flex flex-col justify-center align-center max-w-xl w-full p-8">
+  <div
+    v-else
+    class="flex flex-col justify-center align-center max-w-xl w-full p-8"
+  >
     <v-text-field
-      v-model="selfPlayerTextInput"
+      v-model="guestPlayerTextInput"
       required
-      :rules="[() => !!selfPlayerTextInput || 'This field is required']"
+      :rules="[() => !!guestPlayerTextInput || 'This field is required']"
       label="Full Name"
       placeholder="John Doe"
       variant="outlined"
       class="w-full my-8"
     />
-    <v-btn v-if="!selfPlayer" @click="setSelfPlayerName">
+    <v-btn v-if="!state.player1Name" @click="setGuestReady">
       Start
     </v-btn>
     <h3 v-else>
-      You are joining {{ otherPlayer }} match! The match will be started in
-      {{ Math.max(secondsToStart, 0) }}...
+      You are joining {{ state.player2Name }} match! The match will be started
+      in {{ Math.max(secondsToStart, 0) }}...
     </h3>
   </div>
 </template>
